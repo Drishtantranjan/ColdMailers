3d1e8da2ffe727b52ec4ae868a6cd7b5b4d3ff01
Drishtant Ranjan <drishtantranjan11122000@gmail.com>
2024-01-17 14:53:11 +0530

    login screen & screen util

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:cold_mailing/onboarding/presentation/views/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'auth/presentation/views/splash_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +14,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
 
+        ),
+        home:  Onboarding(),
       ),
-      home:  Onboarding(),
     );
   }
 }
--- lib/onboarding/presentation/views/onboarding_screen.dart
+++ lib/onboarding/presentation/views/onboarding_screen.dart
@@ -1,9 +1,10 @@
-import 'package:cold_mailing/auth/presentation/views/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import '../../data/onboading_data.dart';
 
+import '../../../auth/presentation/views/auth_screen.dart';
+import '../../data/onboading_data.dart';
 
 class Onboarding extends StatefulWidget {
   @override
@@ -28,123 +29,115 @@ class _OnboardingState extends State<Onboarding> {
 
   @override
   Widget build(BuildContext context) {
-
-    double screenWidth = MediaQuery.of(context).size.width;
-    double paddingValue = screenWidth*0.1;
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
         systemOverlayStyle: SystemUiOverlayStyle(
-          // Status bar color
-            statusBarColor: Colors.white,
-
-          // Status bar brightness (optional)
-          statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
-          statusBarBrightness: Brightness.light, // For iOS (dark icons)
+          statusBarColor: Colors.white,
+          statusBarIconBrightness: Brightness.dark,
+          statusBarBrightness: Brightness.light,
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: PageView.builder(
-              controller: _controller,
-              itemCount: contents.length,
-              onPageChanged: (int index) {
-                setState(() {
-                  currentIndex = index;
-                });
-              },
-              itemBuilder: (_, i) {
-                return Padding(
-                  padding: EdgeInsets.all(paddingValue),
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Text(
-                          contents[i].title,
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _controller,
+                itemCount: contents.length,
+                onPageChanged: (int index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                itemBuilder: (_, i) {
+                  return Padding(
+                    padding: EdgeInsets.all(30.sp),
+                    child: Column(
+                      children: [
+                        Center(
+                          child: Text(
+                            contents[i].title,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          textAlign: TextAlign.center, // Align the text to the center horizontally
                         ),
-                      ),
-
-                      SvgPicture.asset(
-                        contents[i].image,
-                        height: MediaQuery.of(context).size.width*0.75,
-                        width: MediaQuery.of(context).size.height*0.75,
-                      ),
-
-                      // SizedBox(height: MediaQuery.of(context).size.height*0.0001),
-                      Text(
-                        contents[i].description,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 13,
-                          color: Colors.grey,
+                        SvgPicture.asset(
+                          contents[i].image,
+                          height: 0.90.sw,
+                          width: 0.90.sh,
                         ),
-                      )
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(
-                contents.length,
-                    (index) => buildDot(index, context),
-              ),
-            ),
-          ),
-          Container(
-            height: 60,
-            margin: EdgeInsets.all(40),
-            width: double.infinity,
-            child:ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.indigo[900]),
-              ),
-              onPressed: () {
-                if (currentIndex == contents.length - 1) {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => SplashScreen(),
+                        Text(
+                          contents[i].description,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
                     ),
                   );
-                }
-                _controller.nextPage(
-                  duration: Duration(milliseconds: 100),
-                  curve: Curves.bounceIn,
-                );
-              },
-              child: Text(
-                currentIndex == contents.length - 1 ? "Continue" : "Next",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white
+                },
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  contents.length,
+                      (index) => buildDot(index, context),
                 ),
               ),
             ),
-
-          )
-        ],
+            Container(
+              height: 60.h,
+              margin: EdgeInsets.all(40),
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                  MaterialStateProperty.all(Colors.indigo[900]),
+                ),
+                onPressed: () {
+                  if (currentIndex == contents.length - 1) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => AuthScreen(),
+                      ),
+                    );
+                  }
+                  _controller.nextPage(
+                    duration: Duration(milliseconds: 100),
+                    curve: Curves.bounceIn,
+                  );
+                },
+                child: Text(
+                  currentIndex == contents.length - 1 ? "Continue" : "Next",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Container buildDot(int index, BuildContext context) {
     return Container(
-      height: 10,
+      height: 10.h,
       width: currentIndex == index ? 25 : 10,
-      margin: EdgeInsets.only(right: 5),
+      margin: EdgeInsets.only(right: 0.02.sw),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         color: Theme.of(context).primaryColor,
