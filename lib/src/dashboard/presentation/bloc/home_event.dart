d1fb28149ffe67e8b3672d6b5054f1c006037fc6
Drishtant Ranjan <drishtantranjan11122000@gmail.com>
2024-01-23 14:32:55 +0530

    home bloc

---
+++ lib/src/dashboard/presentation/bloc/home_event.dart
@@ -0,0 +1,20 @@
+// part of 'home_bloc.dart';
+
+// @immutable
+// sealed class HomeEvent {}
+
+// abstract class SearchEvent extends HomeEvent {
+//    SearchEvent();
+
+//   @override
+//   List<Object?> get props => [];
+// }
+
+// class SetTextFieldTapped extends SearchEvent {
+//   final bool isTapped;
+
+//   const SetTextFieldTapped(this.isTapped);
+
+//   @override
+//   List<Object?> get props => [isTapped];
+// }
